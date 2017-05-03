ecaf29e5edbfd7a950d9a11691d2f36c8058926b
Sabrina Niklaus <sabrinamarlo@Sabrinas-MBP.hsd1.wa.comcast.net>
2017-05-03 07:37:12 -0700

    Worked on question scene transition and display

---
+++ iQuiz/iQuiz/QuestionViewController.swift
@@ -0,0 +1,61 @@
+//
+//  QuestionViewController.swift
+//  iQuiz
+//
+//  Created by Sabrina Niklaus on 5/3/17.
+//  Copyright © 2017 Sabrina Niklaus. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    struct Question {
+        let text: String
+        let answer: Int
+        let answers: [String]
+    }
+    
+    var questions: [String] = [""]
+    var options: [[String]] = [[""]]
+    var answers: [Int] = []
+    var questionIndex: Int = 0
+    var correct = 0
+    
+    @IBOutlet weak var a1: UIButton!
+    @IBOutlet weak var a2: UIButton!
+    @IBOutlet weak var a3: UIButton!
+    @IBOutlet weak var a4: UIButton!
+
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var answerButtons = [a1, a2, a3, a4]
+        
+        questionLabel.text = questions[questionIndex]
+        
+        for i in 0...3 {
+            answerButtons[i]?.setTitle(options[questionIndex][i], for: UIControlState.normal)
+        }
+    }
+
+    @IBAction func answerClicked(_ sender: UIButton) {
+        // if sender.text is the same as options[answers[questionIndex]] then increase num correct
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
--- iQuiz/iQuiz/ViewController.swift
+++ iQuiz/iQuiz/ViewController.swift
@@ -22,6 +22,33 @@ class ViewController: UITableViewController {
         Subject(name: "Science", image: #imageLiteral(resourceName: "science"), description: "f = ma")
     ]
     
+    let questions = ["What is fire?", "Who is Iron Man?", "Who founded the x-men?", "How did Spider-Man get his powers?", "What is 2 + 2?"]
+    
+    let options = [
+        ["One of the four classical elements",
+         "A magical reaction given to us by God",
+         "A band that hasn't yet been discovered",
+         "Fire! Fire! Fire! heh-heh"],
+        ["Tony Stark",
+         "Obadiah Stane",
+         "A rock hit by Megadeth",
+         "Nobody knows"],
+        ["Tony Stark",
+         "Professor X",
+         "The X-Institute",
+         "Erik Lensherr"],
+        ["He was bitten by a radioactive spider",
+         "He ate a radioactive spider",
+         "He is a radioactive spider",
+         "He looked at a radioactive spider"],
+        ["4",
+         "22",
+         "An irrational number",
+         "Nobody knows"]
+    ]
+    
+    let answers = [1, 1, 2, 1, 1]
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -51,6 +78,15 @@ class ViewController: UITableViewController {
         alertController.addAction(OKAction)
         self.present(alertController, animated: true)
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as! QuestionViewController
+        vc.questions = questions
+        vc.options = options
+        vc.answers = answers
+        vc.questionIndex = 0
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
