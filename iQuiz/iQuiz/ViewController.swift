39a8ca2dc350a9127c79a95f43c34d6eae54842f
Sabrina Niklaus <sabrinamarlo@D-173-250-249-61.dhcp4.washington.edu>
2017-05-16 10:45:05 -0700

    json saves to local storage

--- iQuiz/iQuiz/ViewController.swift
+++ iQuiz/iQuiz/ViewController.swift
@@ -10,9 +10,7 @@ import UIKit
 
 class ViewController: UITableViewController {
 
-    var loaded: Bool = false
     var url = "http://tednewardsandbox.site44.com/questions.json"
-    
     var subjects: [String] = []
     var descriptions: [String] = []
     let icons: [UIImage] = [#imageLiteral(resourceName: "science"), #imageLiteral(resourceName: "marvel"), #imageLiteral(resourceName: "math")]
@@ -21,18 +19,58 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getQuizzes()
+        DispatchQueue.global().async {
+            self.getQuizzes()
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
+
     func getQuizzes() {
         let urlString: URL = URL(string: url)!
         URLSession.shared.dataTask(with: urlString) { (data, response, error) in
             if error != nil {
                 print(error!)
+                
+                let savedJson = UserDefaults.standard
+                if (savedJson.value(forKey: "savedJson") != nil) {
+                    let json = savedJson.value(forKey: "savedJson") as! [[String: Any]]
+                    for section in json {
+                        print(section)
+                        let title = section["title"]! as! String
+                        if let subjectQuestions = section["questions"] as? [[String:Any]] {
+                            var quest: [(String,String)] = []
+                            var ans: [[String]] = []
+                            for val in subjectQuestions {
+                                var array = val["answers"]! as! [String]
+                                let answerIndex = Int(val["answer"]! as! String)! - 1
+                                quest.append((val["text"]! as! String, array[answerIndex]))
+                                ans.append(array)
+                            }
+                            self.questions[title] = quest
+                            self.answers[title] = ans
+                        }
+                        self.subjects.append(title)
+                        self.descriptions.append(section["desc"]! as! String)
+                    }
+
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                    
+                }
             } else {
                 do {
                     let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! [[String:Any]]
+                    
+                    // local storage
+                    let localStorage = UserDefaults.standard
+                    localStorage.set(json, forKey: "savedJson")
+                    localStorage.synchronize()
+                    print("saved")
+                    
                     for section in json {
                         let title = section["title"]! as! String
                         if let subjectQuestions = section["questions"] as? [[String:Any]] {
@@ -50,8 +88,9 @@ class ViewController: UITableViewController {
                         self.subjects.append(title)
                         self.descriptions.append(section["desc"]! as! String)
                     }
-                    self.loaded = true
-                    self.tableView.reloadData()
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
                 } catch {
                     print("error serializing JSON \(error)")
                 }
@@ -70,12 +109,10 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
-        if self.loaded {
-            cell.textLabel?.text = subjects[indexPath.row]
-            cell.detailTextLabel?.text = descriptions[indexPath.row]
-            cell.imageView?.image = icons[indexPath.row]
-        }
+
+        cell.textLabel?.text = subjects[indexPath.row]
+        cell.detailTextLabel?.text = descriptions[indexPath.row]
+        cell.imageView?.image = icons[indexPath.row]
     
         return cell
     }
